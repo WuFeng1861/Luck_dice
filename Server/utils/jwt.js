27b5d179c4fdd1dca3c5107b985cc4a9ab3cbafa
WuFeng <1379459026@qq.com>
2024-11-27 14:21:17 +0800

    移除Server另起git

--- Server/utils/jwt.js
+++
@@ -1,9 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-exports.generateToken = (userId) => {
-  return jwt.sign(
-    { userId },
-    process.env.JWT_SECRET,
-    { expiresIn: process.env.JWT_EXPIRES_IN }
-  );
-}; 
