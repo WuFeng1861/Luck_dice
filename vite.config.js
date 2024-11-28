43c71d01688947c0b7639e05008856bc81bde30a
WuFeng <1379459026@qq.com>
2024-11-28 21:10:21 +0800

    logo

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: './',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src')
     }
-  }
+  },
 }) 
